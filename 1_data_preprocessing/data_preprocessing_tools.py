35a8c03861516f6fee92c7745e17455288fd09e3
Ionut Banu <ionutbanu094@gmail.com>
2024-10-12 23:12:29 +0300

    update

--- 1_data_preprocessing/data_preprocessing_tools.py
+++ 1_data_preprocessing/data_preprocessing_tools.py
@@ -54,7 +54,26 @@ y = le.fit_transform(y)
 
 print(y)
 
-"""## Splitting the dataset into the Training set and Test set
+"""## Splitting the dataset into the Training set and Test set"""
 
-## Feature Scaling
-"""
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
+
+print(X_train)
+
+print(X_test)
+
+print(y_train)
+
+print(y_test)
+
+"""## Feature Scaling"""
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train[:, 3:] = sc.fit_transform(X_train[:, 3:])
+X_test[:, 3:] = sc.transform(X_test[:, 3:])
+
+print(X_train)
+
+print(X_test)
