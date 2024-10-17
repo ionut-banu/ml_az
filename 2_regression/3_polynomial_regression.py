ba8c2796e4d520125a70ccf7dc5ae24e9da53411
Ionut Banu <ionutbanu094@gmail.com>
2024-10-17 11:16:00 +0300

    update polynomial regression

--- 2_regression/3_polynomial_regression.py
+++ 2_regression/3_polynomial_regression.py
@@ -30,7 +30,7 @@ lin_reg.fit(X, y)
 """## Training the Polynomial Regression model on the whole dataset"""
 
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree = 2)
+poly_reg = PolynomialFeatures(degree = 4)
 X_poly = poly_reg.fit_transform(X)
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
@@ -44,11 +44,30 @@ plt.xlabel('Position Level')
 plt.ylabel('Salary')
 plt.show()
 
-"""## Visualising the Polynomial Regression results
+"""## Visualising the Polynomial Regression results"""
 
-## Visualising the Polynomial Regression results (for higher resolution and smoother curve)
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg_2.predict(X_poly), color = 'blue')
+plt.title('Truth or Bluf (Polynomial Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
 
-## Predicting a new result with Linear Regression
+"""## Visualising the Polynomial Regression results (for higher resolution and smoother curve)"""
 
-## Predicting a new result with Polynomial Regression
-"""
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+"""## Predicting a new result with Linear Regression"""
+
+lin_reg.predict([[6.5]])
+
+"""## Predicting a new result with Polynomial Regression"""
+
+lin_reg_2.predict(poly_reg.fit_transform([[6.5]]))
