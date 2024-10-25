1aa4411986225e918a5547158ac0ee0f6ea38e46
Ionut Banu <ionutbanu094@gmail.com>
2024-10-25 20:30:10 +0300

    update logistic regression

--- 3_classification/1_logistic_regression.py
+++ 3_classification/1_logistic_regression.py
@@ -9,20 +9,46 @@ Original file is located at
 # Logistic Regression
 
 ## Importing the libraries
+"""
 
-## Importing the dataset
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
-## Splitting the dataset into the Training set and Test set
+"""## Importing the dataset"""
 
-## Feature Scaling
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
 
-## Training the Logistic Regression model on the Training set
+"""## Splitting the dataset into the Training set and Test set"""
 
-## Predicting a new result
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
-## Predicting the Test set results
+"""## Feature Scaling"""
 
-## Making the Confusion Matrix
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+"""## Training the Logistic Regression model on the Training set"""
+
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train)
+
+"""## Predicting a new result"""
+
+print(classifier.predict(sc.transform([[30, 87000]])))
+
+"""## Predicting the Test set results"""
+
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
+"""## Making the Confusion Matrix
 
 ## Visualising the Training set results
 
